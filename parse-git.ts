2ffcc149b7b0922e6a94fcbe3f2a094f47b0ba2d
duart38 <duartsnel@icloud.com>
2022-12-12 15:42:33 +0100

    [fix] fmt
    Signed-off-by: duart38 <duartsnel@icloud.com>

--- parse-git.ts
+++ parse-git.ts
@@ -19,66 +19,82 @@
  * The output directory to put the parsed files in.
  * > please don't use trailing slashes within the url. the program will do this for you.
  */
-const OUTPUT_DIR = "./snapshots"
+const OUTPUT_DIR = "./snapshots";
 
 /**
  * Indicates if the program should ignore already existing files in the OUTPUT_DIR when the same commit was found
  */
-const IGNORE_EXISTING_FILES = true
+const IGNORE_EXISTING_FILES = true;
 
 /**
  * The file extension of the output files generated within OUTPUT_DIR
  */
-const FILE_EXTENSION = ".json"
+const FILE_EXTENSION = ".json";
 
 type TGitLogCommits = {
-    commit: string,
-    author: {
-      name: string,
-      email: string,
-      date: string
-    }
-}[]
+  commit: string;
+  author: {
+    name: string;
+    email: string;
+    date: string;
+  };
+}[];
 
 // ensure the snapshots folder exists.
-Deno.mkdirSync(OUTPUT_DIR, {recursive: true})
-
+Deno.mkdirSync(OUTPUT_DIR, { recursive: true });
 
 /**
  * Resolves true if a file exists; false otherwise
  */
 async function fileExists(path: string) {
-    return await Deno.stat(path).then(() => true).catch(() => false);
+  return await Deno.stat(path).then(() => true).catch(() => false);
 }
 
 /**
  * Filters out commits that are not from the flat data tool-set
  */
-function onlyFlatData(c: TGitLogCommits[number]){
-    return c.author.email === "flat-data@users.noreply.github.com"
+function onlyFlatData(c: TGitLogCommits[number]) {
+  return c.author.email === "flat-data@users.noreply.github.com";
 }
 
-const RAW_OUT = new TextDecoder().decode(await Deno.run({
-    cmd: ["git", "log", `--pretty=format:{%n  "commit": "%H",%n  "author": {%n    "name": "%aN",%n    "email": "%aE",%n    "date": "%aD"%n  }%n },`],
-    stdout: 'piped'
-}).output())
+const RAW_OUT = new TextDecoder().decode(
+  await Deno.run({
+    cmd: [
+      "git",
+      "log",
+      `--pretty=format:{%n  "commit": "%H",%n  "author": {%n    "name": "%aN",%n    "email": "%aE",%n    "date": "%aD"%n  }%n },`,
+    ],
+    stdout: "piped",
+  }).output(),
+);
 
-const commitHistory = (JSON.parse("["+RAW_OUT.substring(0, RAW_OUT.length - 1)+"]") as TGitLogCommits).filter(onlyFlatData)
+const commitHistory = (JSON.parse(
+  "[" + RAW_OUT.substring(0, RAW_OUT.length - 1) + "]",
+) as TGitLogCommits).filter(onlyFlatData);
 
 /**
  * Writes a snapshot entry based on the commit information
  */
-async function writeSnapshotFile(commit: TGitLogCommits[number]){
-    const COMMIT_DATA = new TextDecoder().decode(await Deno.run({
-        cmd: ["git", "cat-file", "-p", `${commit.commit}:data.json`],
-        stdout: 'piped'
-    }).output())
+async function writeSnapshotFile(commit: TGitLogCommits[number]) {
+  const COMMIT_DATA = new TextDecoder().decode(
+    await Deno.run({
+      cmd: ["git", "cat-file", "-p", `${commit.commit}:data.json`],
+      stdout: "piped",
+    }).output(),
+  );
 
-    Deno.writeTextFileSync(`${OUTPUT_DIR}/${commit.commit}${FILE_EXTENSION}`, `{"commit": "${commit.commit}", "data": ${COMMIT_DATA}}`)
+  Deno.writeTextFileSync(
+    `${OUTPUT_DIR}/${commit.commit}${FILE_EXTENSION}`,
+    `{"commit": "${commit.commit}", "data": ${COMMIT_DATA}}`,
+  );
 }
 
-for(const commit of commitHistory){
-    if(!IGNORE_EXISTING_FILES || (IGNORE_EXISTING_FILES && !(await fileExists(`${OUTPUT_DIR}/${commit.commit}${FILE_EXTENSION}`)))){
-       writeSnapshotFile(commit)
-    }
+for (const commit of commitHistory) {
+  if (
+    !IGNORE_EXISTING_FILES ||
+    (IGNORE_EXISTING_FILES &&
+      !(await fileExists(`${OUTPUT_DIR}/${commit.commit}${FILE_EXTENSION}`)))
+  ) {
+    writeSnapshotFile(commit);
+  }
 }
